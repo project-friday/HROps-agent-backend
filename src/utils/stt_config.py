7230e15400852a00ae0cc9c91b5d06c015a6aa50
Hash-if-renan <hashif@renan.one>
2025-10-07 20:34:08 +0530

    feat: add new names

--- src/utils/stt_config.py
+++ src/utils/stt_config.py
@@ -62,17 +62,42 @@ EMAIL_KEYTERMS = [
     "i cloud dot com",
 ]
 NAME_KEYTERMS = [
-    "Rajdeep","Hashif","Samyak","Ravi", "Sujon", "Hrashal","Naman",
-    "John","Emily","Michael","Sarah","Priya","Riya",
-    "singh","patil","doe","smith","johnson","wilson", "sekh",
-    "Matthew", "Christopher", "Andrew", "Jacob", "walker", "mehta", "harris", "wilson", "miller"
+    "Rajdeep",
+    "Hashif",
+    "Samyak",
+    "Ravi",
+    "Sujon",
+    "Harshal",
+    "Naman",
+    "John",
+    "Emily",
+    "Michael",
+    "Sarah",
+    "Priya",
+    "Riya",
+    "singh",
+    "patil",
+    "doe",
+    "smith",
+    "johnson",
+    "wilson",
+    "sekh",
+    "Matthew",
+    "Christopher",
+    "Andrew",
+    "Jacob",
+    "walker",
+    "mehta",
+    "harris",
+    "wilson",
+    "miller",
+    "khetan",
 ]
 
 
 def make_deepgram_stt(
     language: str = "en-US", endpointing_ms: int = 200, use_keyterms: bool = True
 ) -> deepgram.STT:
-
     """
     language: use "en-IN" for Indian English or "en-US" for US speakers
     endpointing_ms: ~250–350 is a good starting band to avoid mid-email cutoffs
@@ -89,4 +114,3 @@ def make_deepgram_stt(
         stt_kwargs["keyterms"] = EMAIL_KEYTERMS + NAME_KEYTERMS
 
     return deepgram.STT(**stt_kwargs)
-
